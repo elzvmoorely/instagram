847ef006eb5cbd9f677d040a40b4615ac32a2271
elizabethvega <20223l001092@utcv.edu.mx>
2024-10-08 00:04:59 -0600

    Primera subida del proyecto Instagram

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader,IonAvatar, IonTitle, IonToolbar, IonLabel, IonButton, IonImg, IonBadge, IonGrid, IonRow, IonCol, IonIcon, IonItem, IonText, IonCard } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
-})
+  imports: [IonCard, IonText, IonItem, IonIcon, IonCol,IonAvatar, IonRow, IonGrid, IonAvatar, IonBadge, IonImg, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]})
 export class HomePage {
   constructor() {}
 }
